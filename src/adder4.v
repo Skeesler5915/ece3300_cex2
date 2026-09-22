2bc9eb6f86643af387e2997fe66a6fe47671bcba
Skeesler5915 <skeesler5915@gmail.com>
2026-09-21 17:21:11 -0700

    Fix result indexing in adder4.v

--- src/adder4.v
+++ src/adder4.v
@@ -18,11 +18,11 @@ wire cout3;
 	fulladd addc0 
 	(.x(a[0]), .y(b[0]), .cin(1'b0),.sum(result[0]),.cout(cout0));
 	fulladd addc1 
-	(.x(a[1]), .y(b[1]), .cin(cout0),.sum(result[0]),.cout(cout1));
+	(.x(a[1]), .y(b[1]), .cin(cout0),.sum(result[1]),.cout(cout1));
 	fulladd addc2 
-	(.x(a[2]), .y(b[2]), .cin(cout1),.sum(result[0]),.cout(cout2));
+	(.x(a[2]), .y(b[2]), .cin(cout1),.sum(result[2]),.cout(cout2));
 	fulladd addc3 
-	(.x(a[3]), .y(b[3]), .cin(cout2),.sum(result[0]),.cout(cout3));
+	(.x(a[3]), .y(b[3]), .cin(cout2),.sum(result[3]),.cout(cout3));
 	
 // add your code here -- you must use four instances of the full adder "fulladd", defined below
    
